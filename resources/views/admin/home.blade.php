7c325f9133265dc2c8fa274966b1effbba797071
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-07-05 14:36:52 +0200

    Completed: 1-10 ToDoList elements

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Backend Administrator
                 </div>
             </div>
         </div>
